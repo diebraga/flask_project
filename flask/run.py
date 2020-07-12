a2b12b01b07bc279ce1fe9baf8725928df361957
unknown <diebraga0055@gmail.com>
2020-07-12 18:25:49 +0100

    added routes and contact us template

--- flask/run.py
+++ flask/run.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# Routes in the app
 @app.route('/')
-def hello():
+def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
 
 if __name__ == "__main__":
     app.run()
